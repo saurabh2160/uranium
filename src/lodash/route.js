7f4a3a8eb3942ba096fcd603e8634c34e649c16d
saurabh2160 <saurabhmanohar90@gmail.com>
2022-04-07 10:59:39 +0530

    yes

--- src/lodash/route.js
+++ src/lodash/route.js
@@ -5,7 +5,7 @@ const _=require('lodash')
 const router = express.Router();
 
 router.get('/hello-me', function (req, res) {
-    console.log(_.chunk(['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec'],2));
+    console.log(_.chunk(['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec'],2))
     res.send('My first ever api!')
 });
 module.exports = router;
